7fd1c1d3fbd397d657eaefc1eab6ade065a55ea4
Mila Votradovec <mila.votradovec@aisle.com>
2025-12-04 18:18:51 +0100

    day 4 - refactored so I am not ashamed

--- day4.zig
+++ day4.zig
@@ -58,7 +58,7 @@ pub fn part1(lines:  [][] const u8) !i32 {
 
 
 
-const Coords = struct {x: usize, y:usize};
+const Coords = struct {x: i32, y: i32};
 
 pub fn part2(lines:  [][] const u8) !i32 {
     var res: i32 = 0;
@@ -73,37 +73,34 @@ pub fn part2(lines:  [][] const u8) !i32 {
 
     for (0..lines.len) |y| {
         for (0..lines[y].len) |x| {
-            try grid.put(Coords{.x = x, .y = y}, lines[y][x]);
+            try grid.put(Coords{.x = @intCast(x), .y = @intCast(y)}, lines[y][x]);
         }
     }
 
     while (moved) {
         moved = false;
-        for (0..lines.len) |y| {
-            for (0..lines[y].len) |x| {
-                if (grid.get(Coords{.x= x, .y= y}) != '@') continue;
-                if (isMovable2(grid, y, x)) {
-                    moved = true;
-                    res += 1;
-                    // this is fine, we don't have to remove and wait for next iteration
-                    try grid.put(Coords {.x=x, .y=y}, '.');
-                }
+        var it =grid.iterator();
+        while (it.next()) |e| {
+            const k = e.key_ptr.*;
+            const v = e.value_ptr.*;
+            if (v != '@') continue;
+            if (isMovable2(grid, k.y, k.x)) {
+                moved = true;
+                res += 1;
+                // this is fine, we don't have to remove and wait for next iteration
+                try grid.put(k, '.');
             }
         }
     }
     return res;
 }
 
-fn isMovable2(lines: std.AutoArrayHashMap(Coords, u8), y: usize, x: usize) bool {
+fn isMovable2(lines: std.AutoArrayHashMap(Coords, u8), y: i32, x: i32) bool {
     var r: i32 = 0;
     for ([_]i32{ -1, 0, 1 }) |dx| {
         for ([_]i32{ -1, 0, 1 }) |dy| {
-            const yy:i32 = @intCast(y);
-            const xx:i32 = @intCast(x);
-            if (((yy + dy) < 0) or ((xx + dx) < 0) or (dy == 0 and dx == 0)) continue;
-            const yyy: usize = @intCast(yy+dy);
-            const xxx: usize = @intCast(xx+dx);
-            if (lines.get(Coords{.x= xxx, .y= yyy}) == '@') r += 1;
+            if (dy == 0 and dx == 0) continue;
+            if (lines.get(Coords{.x= x+dx, .y= y+dy}) == '@') r += 1;
         }
     }
     return (r < 4);
